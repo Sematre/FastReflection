8c24b405ea790400387bb3291424e3377edc64b3
Sematre <Sematre@gmx.de>
2021-02-06 09:48:36 +0100

    Update JUnit to '5.6.3'

--- src/test/java/de/sematre/fastreflection/FastReflectionTest.java
+++ src/test/java/de/sematre/fastreflection/FastReflectionTest.java
@@ -1,10 +1,10 @@
 package de.sematre.fastreflection;
 
-import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import de.sematre.fastreflection.util.TestClass;
 
